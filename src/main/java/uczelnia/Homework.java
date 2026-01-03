f98c3f1cb130dc0d78afca28d0ce89a51b6b7d28
donutalex <75425751+DonutAlex@users.noreply.github.com>
2026-01-03 14:53:17 +0100

    Added homework and solvedhomework classes

---
+++ src/main/java/uczelnia/Homework.java
@@ -0,0 +1,43 @@
+package uczelnia;
+
+public class Homework {
+    private String title;
+    private String description;
+    private Lecturer author;
+    private int maxPoints;
+
+    public Homework(String title, String description, Lecturer author, int maxPoints){
+        this.title = title;
+        this.description = description;
+        this.author = author;
+        this.maxPoints = maxPoints;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public Lecturer getAuthor() {
+        return author;
+    }
+
+    public int getMaxPoints() {
+        return maxPoints;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setMaxPoints(int maxPoints) {
+        this.maxPoints = maxPoints;
+    }
+}
---
+++ src/main/java/uczelnia/SolvedHomework.java
@@ -0,0 +1,34 @@
+package uczelnia;
+
+public class SolvedHomework extends Homework{
+    private String solution;
+    private Student solutionAuthor;
+
+    public SolvedHomework(String title, String description, Lecturer author, int maxPoints, String solution, Student solutionAuthor){
+        super(title, description, author, maxPoints);
+        this.solution = solution;
+        this.solutionAuthor = solutionAuthor;
+    }
+    public SolvedHomework(Homework homework, String solution, Student solutionAuthor)
+    {
+        super(homework.getTitle(), homework.getDescription(), homework.getAuthor(), homework.getMaxPoints());
+        this.solution = solution;
+        this.solutionAuthor = solutionAuthor;
+    }
+
+    public String getSolution() {
+        return solution;
+    }
+
+    public void setSolution(String solution) {
+        this.solution = solution;
+    }
+
+    public Student getSolutionAuthor() {
+        return solutionAuthor;
+    }
+
+    public void setSolutionAuthor(Student solutionAuthor) {
+        this.solutionAuthor = solutionAuthor;
+    }
+}
